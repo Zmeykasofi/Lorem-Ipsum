372bb5b4d95ab0f49139d00a5d41f3e004d1962c
Софья Полянкина <zmeykasofi@mail.ru>
2022-08-11 16:19:31 +0700

    Text aligned, IsAlphabetic added

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,31 @@ import java.util.Map;
 public class Main {
 
     public static String setText() {
-        String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut"
+        + "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut"
+        + "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum"
+        + "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia"
+        + "deserunt mollit anim id est laborum.";
+
         String textLowerCase = text.toLowerCase();
         return textLowerCase.replaceAll("\\s", "");
     }
 
     public static void main(String[] args) {
+
         char[] letters = setText().toCharArray();
         Map<Character, Integer> map = new HashMap<>();
 
         for (int i = 0; i < letters.length; i++) {
             char symbol = setText().charAt(i);
-            if (!map.containsKey(symbol)) {
-                map.put(symbol, 1);
-            } else {
-                int value = map.get(symbol);
-                value++;
-                map.put(symbol, value);
+            if (Character.isAlphabetic(symbol)) {
+                if (!map.containsKey(symbol)) {
+                    map.put(symbol, 1);
+                } else {
+                    int value = map.get(symbol);
+                    value++;
+                    map.put(symbol, value);
+                }
             }
         }
         int maxValue = -1;
